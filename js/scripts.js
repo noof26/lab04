7e12ceb8642193c53e418ec9635ed56e743a8522
p16225481 <p16225481@my365.dmu.ac.uk>
2020-02-11 15:00:14 +0000

    new lab 4 work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = () => {menuNav.classList.toggle('open')}
+
+menuToggle.addEventListener('click',toggleMenu);
